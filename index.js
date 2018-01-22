8b696f405761dfc5d9b02bd9b5813e5e72412308
jkushner18@gmail.com Kushner <jkushner18@gmail.com>
2018-01-22 20:39:34 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function theBeatlesPlay(musicians, instruments){
+
+var complete = [];
+
+for(let i = 0; i<musicians.length; i++){
+
+complete.push(musicians[i] + " plays " + instruments[i])
+
+ 
+
+}
+return complete
+
+}
+
